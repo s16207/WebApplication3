9533a85493a9100076ce65499ff354764743dbd1
GregorGolen <gregorgolen@gmail.com>
2021-01-29 18:41:01 +0100

    Model

---
+++ WebApplication3/Controllers/StudentsController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication3.Controllers
+{
+    [Route("api/students")]
+    [ApiController]
+    public class StudentsController : Controller
+    {
+        [HttpGet]
+        public string GetStudents()
+        {
+            return "Alicja, Patryk, Kupa";
+        }
+        [HttpGet("{id}")]
+        public string GetStudent(int id)
+        {
+            if(id == 1)
+            {
+                return "Jan Jeden";
+            }
+
+            return "Not found";
+        }
+
+    }
+}
